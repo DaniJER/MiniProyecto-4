e0a2cf9a978b1051f3b456a264cf56d9578d21bd
Daniel <danicraft.op7@gamil.com>
2023-11-30 13:57:24 -0500

    first commit

---
+++ src/main/java/com/mycompany/mini/proyecto3/MiniProyecto3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mini.proyecto3;
+
+/**
+ *
+ * @author El Rey
+ */
+public class MiniProyecto3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
